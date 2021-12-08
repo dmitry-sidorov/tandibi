d86f1e93cb5934656ea7ac1f8d254a1472474f3c
Dmitriy Sidorov <d_sidorov@wargaming.net>
2021-12-08 23:01:33 +0300

    Plug in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
+  before_action :configure_devise_params, if: :devise_controller?
+
+  protected
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :first_name,
+      :last_name,
+      :username,
+      :email,
+      :password,
+      :password_confirmation
+    ])
+  end
 
   private
   def layout_by_resource
